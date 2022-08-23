182d81a697bdc15fe07e67d610d1f5cbaa944392
limetimeline <kjhh0608@gmail.com>
2022-08-23 22:32:58 +0900

    Update dashboard structures

---
+++ app/src/main/java/com/example/arambyeol/ui/dashboard/BoardData.kt
@@ -0,0 +1,6 @@
+package com.example.arambyeol.ui.dashboard
+
+data class BoardData
+    (var id: String,
+     val name: String)
+
--- app/src/main/java/com/example/arambyeol/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/arambyeol/ui/dashboard/DashboardFragment.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.arambyeol.databinding.FragmentDashboardBinding
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 class DashboardFragment : Fragment() {
 
@@ -32,6 +33,39 @@ class DashboardFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+
+        val viewBoard = binding.board
+        val list = ArrayList<BoardData>()
+        list.add(BoardData("1", "name 1"))
+        list.add(BoardData("2", "name 2"))
+        list.add(BoardData("3", "name 3"))
+        list.add(BoardData("4", "name 4"))
+        list.add(BoardData("5", "name 5"))
+        list.add(BoardData("6", "name 6"))
+        list.add(BoardData("7", "name 7"))
+        list.add(BoardData("8", "name 8"))
+        list.add(BoardData("9", "name 9"))
+        list.add(BoardData("10", "name 10"))
+        list.add(BoardData("11", "name 11"))
+        list.add(BoardData("12", "name 12"))
+
+
+        val adapter = RecyclerUserAdapter(list)
+        viewBoard.adapter = adapter
+
+
+//        val list = ArrayList<BoardData>()
+//        list.add(BoardData("1", "name 1"))
+//        list.add(BoardData("2", "name 2"))
+//        list.add(BoardData("3", "name 3"))
+//
+//        val adapter = RecyclerUserAdapter(list)
+//        board.adapter = adapter
+
+
+
+
         return root
     }
 
